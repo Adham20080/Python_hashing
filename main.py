bda6457a0304ba46b8ffc0852a259b24f10757f2
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-02-16 20:37:53 +0500

    Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import hashlib
+
+# print(hashlib.algorithms_available)
+
+data365 = "Ahmadjon 2008"
+
+# hashing = hashlib.md5(str(data365).encode("utf-8")).hexdigest()
+# print(hashing)
+
+# hashing1 = hashlib.sha256(str(data365).encode("utf-8")).hexdigest()
+# hashing2 = hashlib.sha384(str(data365).encode("utf-8")).hexdigest()
+# hashing3 = hashlib.md5(str(data365).encode("utf-8")).hexdigest()
+# hashing4 = hashlib.blake2b(str(data365).encode("utf-8")).hexdigest()
+# hashing5 = hashlib.sha1(str(data365).encode("utf-8")).hexdigest()
+
+
+# hash_data: list = [hashing1, hashing2, hashing3, hashing4, hashing5]
+# for i in hash_data:
+#     print(i)
+
+# def hashing_func(abcde: str):
+#     sha256 = hashlib.sha256(str(abcde).encode("utf-8")).hexdigest()
+#     return sha256
+#
+#
+# print(hashing_func(input("Kirit: ")))
